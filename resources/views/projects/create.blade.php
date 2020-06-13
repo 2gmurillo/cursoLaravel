61379eed395e49cc9876c9c1cd6360eb7abab26e
2gmurillo <2gmurillo@gmail.com>
2020-06-13 16:37:58 -0500

    feat: reutilizando el formulario

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,13 +3,7 @@
 <h1>{{__('Create a new project')}}</h1>
 
 <form class="form" action="{{ route('projects.store') }}" method="POST">
-  @csrf
-  <input class="input" type="text" name="title" placeholder="@lang('The project title')" value="{{old('title')}}">
-  {!! $errors->first('title', '<p class="error-msg">:message</p>') !!}
-  <input class="input" type="text" name="url" placeholder="@lang('The project url')" value="{{old('url')}}">
-  {!! $errors->first('url', '<p class="error-msg">:message</p>') !!}
-  <textarea class="textarea" name="description" placeholder="@lang('Description')...">{{old('description')}}</textarea>
-  {!! $errors->first('description', '<p class="error-msg">:message</p>') !!}
-  <button class="button" type="submit">@lang('Save')</button>
+  @include('projects._form', ['buttonText'=>'Save'])
+
 </form>
 @endsection
